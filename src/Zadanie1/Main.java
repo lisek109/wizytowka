b295f7b5ba62db7726273d611bc295f0ac83e01d
lisek109 <lis109@wp.pl>
2021-02-09 18:37:25 +0100

    moj pierwszy commit

---
+++ src/Zadanie1/Main.java
@@ -0,0 +1,26 @@
+package Zadanie1;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Wybierz opcje");
+            System.out.println("1. Wyswietl gosi");
+            System.out.println("2. Dodaj goscia");
+            System.out.println("3. Wyswietl potrawy");
+            System.out.println("4. Znajdz po numerze telefonu");
+            System.out.println("5. Wyjscie");
+
+            int userCoice = scanner.nextInt();
+
+
+        }
+    }
+
+}
+
+
+
